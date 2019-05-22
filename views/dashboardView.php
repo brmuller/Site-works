ac9bdb72abf42e42353c3418131e9bef4c376cc1
brmuller <b.muller727@gmail.com>
2019-05-22 18:24:42 +0200

    changement logo download DB

--- views/dashboardView.php
+++ views/dashboardView.php
@@ -96,7 +96,7 @@
     <div class="ui grid"  id="team-stats" style="margin-top:10px;">
       <div class="twelve wide column">
         <h3 class="ui dividing header">Tâches</h3>
-        <a href="/workflow/dashboard.php?type=exporttasks&id=<?= $team ?>" class="item"><i class="file excel icon"></i></a>
+        <a href="/workflow/dashboard.php?type=exporttasks&id=<?= $team ?>" class="item"><i class="large cloud download icon"></i></a>
       </div>
       <div class="four wide column">
         <div class="ui tiny statistics" style="float:right;">
